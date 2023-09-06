afe03cddf2347eb287625c68f7a7f7f01aa86cc2
Ryan Snyder <ryansnyder.me@gmail.com>
2023-09-06 11:26:49 +0100

    Replace Google UA/GT4

--- js/js.js
+++ js/js.js
@@ -1,8 +1,7 @@
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-17772133-1']);
-_gaq.push(['_trackPageview']);
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-VM6V951HX0"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-VM6V951HX0');
+</script>
